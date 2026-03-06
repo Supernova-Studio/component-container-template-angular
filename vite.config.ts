cd982a836447fe68e5ceae573ea48cc3b1e9c5b5
Sergii Golyshev <sergii@supernova.io>
2026-03-06 10:17:06 +0200

    RCT-8235: Change output dir

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { resolve } from "path";
 export default defineConfig({
   base: "./",
   plugins: [angular()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     port: 3000,
     allowedHosts: true,
